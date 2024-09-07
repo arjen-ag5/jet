5d0cd247723acd935301dd7a7b562ff91f56697a
Arjen Brouwer <arjen@ag5.com>
2024-09-07 20:14:38 +0200

    Use CustomExpression

--- internal/jet/expression.go
+++ internal/jet/expression.go
@@ -316,30 +316,27 @@ func (s *complexExpression) serialize(statement StatementType, out *SQLBuilder, 
 	}
 }
 
-type arraySubscriptExpression struct {
-	ExpressionInterfaceImpl
-	array     Expression
-	subscript IntegerExpression
-}
-
-func (a arraySubscriptExpression) serialize(statement StatementType, out *SQLBuilder, options ...SerializeOption) {
-	if !contains(options, NoWrap) {
-		out.WriteString("(")
-	}
-	a.array.serialize(statement, out, FallTrough(options)...) // FallTrough here because complexExpression is just a wrapper
-	out.WriteString("[")
-	a.subscript.serialize(statement, out, FallTrough(options)...) // FallTrough here because complexExpression is just a wrapper
-	out.WriteString("]")
-	if !contains(options, NoWrap) {
-		out.WriteString(")")
-	}
-}
+//type arraySubscriptExpression struct {
+//	ExpressionInterfaceImpl
+//	array     Expression
+//	subscript IntegerExpression
+//}
+//
+//func (a arraySubscriptExpression) serialize(statement StatementType, out *SQLBuilder, options ...SerializeOption) {
+//	if !contains(options, NoWrap) {
+//		out.WriteString("(")
+//	}
+//	a.array.serialize(statement, out, FallTrough(options)...) // FallTrough here because complexExpression is just a wrapper
+//	out.WriteString("[")
+//	a.subscript.serialize(statement, out, FallTrough(options)...) // FallTrough here because complexExpression is just a wrapper
+//	out.WriteString("]")
+//	if !contains(options, NoWrap) {
+//		out.WriteString(")")
+//	}
+//}
 
 func arraySubscriptExpr(array Expression, subscript IntegerExpression) Expression {
-	arraySubscriptExpression := &arraySubscriptExpression{array: array, subscript: subscript}
-	arraySubscriptExpression.ExpressionInterfaceImpl.Parent = arraySubscriptExpression
-
-	return arraySubscriptExpression
+	return CustomExpression(array, Token("["), subscript, Token("]"))
 }
 
 type skipParenthesisWrap struct {
